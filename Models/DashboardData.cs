a2c2ab7313cf00ac463d31ff5dc4a09b4c7a1216
Allison <st10083717@vcconnect.edu.za>
2025-04-08 20:09:40 +0200

    Added Telerik UI for Blazor Components and added the sidebar navigation menu

---
+++ Models/DashboardData.cs
@@ -0,0 +1,10 @@
+﻿namespace DashBoardMockUp.Models
+{
+    public class DashboardData
+    {
+        public decimal TotalSales { get; set; }
+        public decimal AverageSale { get; set; }
+        public int TransactionCount { get; set; }
+  
+    }
+}
